3e4934d73bbc2e15eeeccdf0cb13fc19236483d3
Michael Van Kirk <vankirk.6@wright.edu>
2021-02-24 13:30:47 -0500

    first commit

---
+++ src/main/Customer.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Customer {
+
+    private double arrivalTime;
+    private int numItems;
+    private double timePerItem;
+
+    public Customer(double arrivalTime, int numItems, double timePerItem){
+        this.arrivalTime = arrivalTime;
+        this.numItems = numItems;
+        this.timePerItem = timePerItem;
+    }
+
+    public double getArrivalTime(){
+        return this.arrivalTime;
+    }
+
+    public int getNumItems(){
+        return this.numItems;
+    }
+
+    public double getTimePerItem(){
+        return this.timePerItem;
+    }
+}
